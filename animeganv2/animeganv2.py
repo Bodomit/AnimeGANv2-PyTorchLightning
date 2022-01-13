d2ab8a0bd73cb0e60c36943032e34fa142b49c1e
Glen Brown <gbrown29@qub.ac.uk>
2022-01-13 23:29:09 +0000

    Validation Loop (#2)
    * Basic (untested) validation loop.
    
    * Fix full MVP training loop
    
    * Logging validation images.
    
    * Ignore lightning logs.
    
    * Drop last fix.
    
    * Not calculating grad for feature model.
    
    * Reducing val ratoin to 0.05

--- animeganv2/animeganv2.py
+++ animeganv2/animeganv2.py
@@ -6,14 +6,19 @@ import torch
 import torchvision
 from icecream import ic
 from torch import nn
+from torchvision.transforms.functional import to_pil_image
+from torchvision.utils import make_grid
 
 from .losses import DiscriminatorLoss, GeneratorLoss, InitLoss
 from .models import Discriminator, Generator
 
 
 class AnimeGanV2(pl.LightningModule):
-    def __init__(self) -> None:
+    def __init__(self, init_epochs: int) -> None:
         super().__init__()
+        self.save_hyperparameters()
+        self.init_epochs = init_epochs
+
         self.automatic_optimization = False
 
         self.generator = Generator()
@@ -30,11 +35,12 @@ class AnimeGanV2(pl.LightningModule):
         # Monkey-patch to stop forward path through classifier
         def forward(self, x: torch.Tensor) -> torch.Tensor:
             x = self.features(x)
-            x = self.avgpool(x)
-            x = torch.flatten(x, 1)
             return x
 
         vgg19.forward = types.MethodType(forward, vgg19)
+
+        # Don't compute grads for feature model.
+        vgg19.requires_grad_(False)
 
         return vgg19
 
@@ -52,34 +58,62 @@ class AnimeGanV2(pl.LightningModule):
 
         generated = self.generator(real)
 
-        # Initilaize the generator on the first epoch.
-        if self.current_epoch < 1:
+        # Initilaize the generator on the first n epochs.
+        if self.current_epoch < self.init_epochs:
             i_loss = self.init_loss((real, generated))
-            init_opt.zero_grad()
+            init_opt.zero_grad()  # type: ignore
             self.manual_backward(i_loss)
             init_opt.step()
             return
 
         # Update the generator.
         generated_logit = self.discriminator(generated)
-        g_loss = self.generator_loss(real, anime_gray, generated, generated_logit)
+        g_loss = self.generator_loss((real, anime_gray, generated, generated_logit))
 
-        g_opt.zero_grad()
+        g_opt.zero_grad()  # type: ignore
         self.manual_backward(g_loss)
         g_opt.step()
 
         # Update the discriminator.
+        generated = self.generator(real)
+        generated_logit = self.discriminator(generated)
         real_logit = self.discriminator(real)
         anime_gray_logit = self.discriminator(anime_gray)
         smooth_logit = self.discriminator(anime_smooth)
 
         d_loss = self.discriminator_loss(
-            real_logit, anime_gray_logit, generated_logit, smooth_logit
+            (real_logit, anime_gray_logit, generated_logit, smooth_logit)
         )
 
-        d_opt.zero_grad()
+        d_opt.zero_grad()  # type: ignore
         self.manual_backward(d_loss)
         d_opt.step()
+
+    def validation_step(self, batch, batch_idx):
+        generated = self.generator(batch)
+        image_pairs = torch.stack((batch, generated), dim=1)
+        image_pairs = torch.flatten(image_pairs, start_dim=0, end_dim=1)
+
+        # Undo linear standardisation
+        image_pairs = (image_pairs + 1.0) * 127.5
+        image_pairs = torch.clamp(image_pairs, 0, 255)
+        image_pairs = image_pairs.to(torch.uint8)
+
+        return image_pairs
+
+    def validation_epoch_end(self, generated_batches):
+
+        if isinstance(generated_batches, list):
+            images = torch.concat(generated_batches, 0)
+        elif isinstance(generated_batches, torch.Tensor):
+            images = generated_batches
+        else:
+            raise ValueError
+
+        # Create image grid and log to tensorboard.
+        grid_image = make_grid(images)
+        tensorboard = self.logger.experiment  # type: ignore
+        tensorboard.add_image("validation", grid_image, self.current_epoch)
 
     def configure_optimizers(self):
         return [
--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+import argparse
+
 import pytorch_lightning as pl
 from pytorch_lightning import callbacks
 
@@ -9,10 +11,11 @@ def main(args):
     data = AnimeGanDataModule(
         "/mnt/d/datasets/AnimeGANDataset/train_photo/",
         "/mnt/d/datasets/AnimeGANDataset/Shinkai/",
-        2,
+        args.batch_size,
+        args.val_batch_size,
     )
 
-    model = AnimeGanV2()
+    model = AnimeGanV2(args.init_epochs)
 
     trainer = pl.Trainer(
         max_epochs=101,
@@ -22,9 +25,23 @@ def main(args):
         multiple_trainloader_mode="max_size_cycle",
         callbacks=[callbacks.ModelSummary(max_depth=2), callbacks.RichProgressBar()],
         detect_anomaly=True,
+        fast_dev_run=args.debug,
     )
     trainer.fit(model, data)
 
 
 if __name__ == "__main__":
-    main(None)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--batch-size", "-b", type=int, default=4)
+    parser.add_argument("--val-batch-size", "-vb", type=int, default=4)
+    parser.add_argument("--init-epochs", type=int, default=1)
+    parser.add_argument("--debug", action="store_true")
+    args = parser.parse_args()
+
+    # Set init epochs to 0 if in debug mode to fast_dev_run through
+    # all losses and models etc.
+    if args.debug:
+        args.init_epochs = 0
+
+    main(args)
